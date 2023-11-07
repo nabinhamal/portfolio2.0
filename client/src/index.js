b74fcef2e62079da6346e4fddafa328854af433f
Nabin Hamal <68802971+nabinhamal@users.noreply.github.com>
2023-11-07 10:11:49 +0545

    Update index.js
    loading screen added

--- client/src/index.js
+++ client/src/index.js
@@ -1,20 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { AdminPanel, App, LoginPage } from "./containers";
 import "./index.css";
 import { AuthProvider } from "./utils/auth";
+import { MainLoader } from "./components"; // Assuming you have a MainLoader component
 
+const Root = () => {
+  const [loading, setLoading] = useState(true);
 
-ReactDOM.render(
-  <AuthProvider>
-    <Router>
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/adminpanel" element={<AdminPanel />} />
-        <Route path="/" element={<App />} />
-      </Routes>
-    </Router>
-  </AuthProvider>,
-  document.getElementById('root')
-);
+  // Simulate loading (You should replace this with your actual loading logic)
+  setTimeout(() => {
+    setLoading(false);
+  }, 2000);
+
+  return (
+    <AuthProvider>
+      <Router>
+        {loading ? (
+          <MainLoader />
+        ) : (
+          <Routes>
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/adminpanel" element={<AdminPanel />} />
+            <Route path="/" element={<App />} />
+          </Routes>
+        )}
+      </Router>
+    </AuthProvider>
+  );
+};
+
+ReactDOM.render(<Root />, document.getElementById('root'));
