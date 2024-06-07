03856ff99e7517dfafda18f127e2b4b8ebd4a5c4
Nabin Hamal <68802971+nabinhamal@users.noreply.github.com>
2024-06-07 13:23:51 +0545

    .

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { AdminPanel, App, LoginPage } from "./containers";
+import App from "./containers/App";
 import "./index.css";
 import { AuthProvider } from "./utils/auth";
 import { MainLoader } from "./components"; // Assuming you have a MainLoader component
-
+const LoginPage = lazy(() => import("./containers/LoginPage"));
+const AdminPage = lazy(() => import("./containers/AdminPanel"));
 const Root = () => {
   const [loading, setLoading] = useState(true);
 
@@ -20,11 +21,13 @@ const Root = () => {
         {loading ? (
           <MainLoader />
         ) : (
-          <Routes>
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/adminpanel" element={<AdminPanel />} />
-            <Route path="/" element={<App />} />
-          </Routes>
+          <Suspense fallback={<Loading />}>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/adminpanel" element={<AdminPage />} />
+          <Route path="/" element={<App />} />
+        </Routes>
+      </Suspense>
         )}
       </Router>
     </AuthProvider>
